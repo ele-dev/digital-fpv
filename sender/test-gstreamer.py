398ceeb00066ed51724f51c847c79303d0e79c75
Elias Geiger <elias@ele-dev.de>
2021-05-24 20:25:39 +0200

    Improved python gstreamer example script

--- sender/test-gstreamer.py
+++ sender/test-gstreamer.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 
+# imports
 import gi
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst, GLib, GObject
 import time
 
-# basic test pipeline
-command = [ 'videotestsrc', '!', 'video/x-raw,width=640,height=480,framerate=30/1', '!',
-	'x264enc', '!', 'rtph264pay', 'pt=96', '!', 'udpsink', 'host=10.66.66.2', 'port=6000' ]
+host = "10.66.66.2"
 
-# create and run the pipeline
+# create and run a basic test pipeline
 Gst.init_check(None)
-pipeline = Gst.parse_launch(f"videotestsrc ! video/x-raw,width=640,height=480,framerate=30/1 ! x264enc ! rtph264pay pt=96 ! udpsink host=10.66.66.2 port=6000")
+pipeline = Gst.parse_launch(f"videotestsrc ! video/x-raw,width=640,height=480,framerate=30/1 ! x264enc ! rtph264pay pt=96 ! udpsink host=" + host + " port=6000")
 print("Pipeline created and launched")
 
-# time.sleep(20)
-# print("Closing pipeline and application ...")
-
+# main loop waiting to be interrupted
 loop = GLib.MainLoop()
 pipeline.set_state(Gst.State.PLAYING)
 try:
@@ -24,4 +21,5 @@ try:
 except Exception as e:
     print(e)
 
+# stop the pipeline before exit
 pipeline.set_state(Gst.State.NULL)
