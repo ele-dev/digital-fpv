2207774a15f7fd088b3797177009bd6a9baf5ebb
Elias Geiger <elias@ele-dev.de>
2021-05-24 19:33:24 +0200

    Completed gstreamer python example script

--- sender/test-gstreamer.py
+++ sender/test-gstreamer.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
-from . import pipeline
-from gi.repository import GObject
 import gi
 gi.require_version('Gst', '1.0')
-from gi.repository import Gst
-
+from gi.repository import Gst, GLib, GObject
+import time
 
 # basic test pipeline
 command = [ 'videotestsrc', '!', 'video/x-raw,width=640,height=480,framerate=30/1', '!',
@@ -13,10 +11,17 @@ command = [ 'videotestsrc', '!', 'video/x-raw,width=640,height=480,framerate=30/
 
 # create and run the pipeline
 Gst.init_check(None)
-pipe = pipeline.Pipe('sample', cmd)
-print("Pipeline created")
-pipe.run()
-print("Pipeline launched")
+pipeline = Gst.parse_launch(f"videotestsrc ! video/x-raw,width=640,height=480,framerate=30/1 ! x264enc ! rtph264pay pt=96 ! udpsink host=10.66.66.2 port=6000")
+print("Pipeline created and launched")
 
+# time.sleep(20)
+# print("Closing pipeline and application ...")
 
+loop = GLib.MainLoop()
+pipeline.set_state(Gst.State.PLAYING)
+try:
+    loop.run()
+except Exception as e:
+    print(e)
 
+pipeline.set_state(Gst.State.NULL)
