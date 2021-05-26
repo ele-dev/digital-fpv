546e031d2fdab08dfd485eeecb8a8b1f6cfba05c
Elias Geiger <elias@ele-dev.de>
2021-05-26 13:49:48 +0200

    switched to v4l2 hardware decoder on raspi 4B

--- receiver/playStream.sh
+++ receiver/playStream.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # HEVC UDP stream 
-gst-launch-1.0 udpsrc port=5000 ! queue ! application/x-rtp,media=video,encoding-name=H264,payload=96 ! rtpjitterbuffer ! rtph264depay ! queue ! h264parse ! omxh264dec ! autovideosink 
+gst-launch-1.0 udpsrc port=5000 ! queue ! application/x-rtp,media=video,encoding-name=H264,payload=96 ! rtpjitterbuffer ! rtph264depay ! queue ! h264parse ! v4l2h264dec ! autovideosink 
 
 # gst-launch-1.0.exe udpsrc port=5000 ! application/x-rtp,media=video,encoding-name=H265,payload=96 ! rtph265depay ! d3d11h265dec ! d3d11videosink
 
