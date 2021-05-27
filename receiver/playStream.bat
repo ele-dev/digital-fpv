2cf3a114ffe4c890032910c63c3754d4997e1f1c
Elias Geiger <elias@ele-dev.de>
2021-05-27 20:59:23 +0200

    Updated windows pipeline to use h264 hardware decoding

--- receiver/playStream.bat
+++ receiver/playStream.bat
@@ -1,7 +1,7 @@
 @echo on
 
 :: HEVC UDP stream 
-gst-launch-1.0.exe udpsrc port=5000 ! queue ! application/x-rtp,media=video,encoding-name=H265,payload=96 ! rtph265depay ! queue ! h265parse ! avdec_h265 ! autovideosink 
+gst-launch-1.0.exe udpsrc port=5000 ! queue ! application/x-rtp,media=video,encoding-name=H264,payload=96 ! rtph264depay ! queue ! h264parse ! d3d11h264dec ! autovideosink 
 
 :: gst-launch-1.0.exe udpsrc port=5000 ! application/x-rtp,media=video,encoding-name=H265,payload=96 ! rtph265depay ! d3d11h265dec ! d3d11videosink
 
