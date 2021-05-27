a2621f51e52d43708c7c5724aeca60693a2250af
Elias Geiger <elias@ele-dev.de>
2021-05-27 21:08:49 +0200

    Enabled jitterbuffer in windows player pipeline

--- receiver/playStream.bat
+++ receiver/playStream.bat
@@ -1,7 +1,7 @@
 @echo on
 
 :: HEVC UDP stream 
-gst-launch-1.0.exe udpsrc port=5000 ! queue ! application/x-rtp,media=video,encoding-name=H264,payload=96 ! rtph264depay ! queue ! h264parse ! d3d11h264dec ! autovideosink 
+gst-launch-1.0.exe udpsrc port=5000 ! queue ! application/x-rtp,media=video,encoding-name=H264,payload=96 ! rtpjitterbuffer ! rtph264depay ! queue ! h264parse ! d3d11h264dec ! autovideosink 
 
 :: gst-launch-1.0.exe udpsrc port=5000 ! application/x-rtp,media=video,encoding-name=H265,payload=96 ! rtph265depay ! d3d11h265dec ! d3d11videosink
 
