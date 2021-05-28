37aca5196032d6fd98f9f8fe4901dd0ff1c9736b
Elias Geiger <elias@ele-dev.de>
2021-05-28 14:48:13 +0200

    correction

--- receiver/playStream.bat
+++ receiver/playStream.bat
@@ -1,7 +1,7 @@
 @echo on
 
 :: HEVC UDP stream 
-gst-launch-1.0.exe udpsrc port=5000 ! queue ! application/x-rtp,media=video,encoding-name=h264,payload=96 ! rtpjitterbuffer ! rtph264depay ! queue ! h264parse ! d3d11h264dec ! autovideosink
+gst-launch-1.0.exe udpsrc port=5000 ! queue ! application/x-rtp,media=video,encoding-name=H264,payload=96 ! rtpjitterbuffer ! rtph264depay ! queue ! h264parse ! d3d11h264dec ! autovideosink
 
 :: HEVC TCP stream (not working yet)
 :: gst-launch-1.0.exe tcpclientsrc host=192.168.178.62 port=5001 ! h264parse ! avdec_h264 ! autovideosink
