badcabb50a5edfeb113d370e5cdfbc72e6905b79
Elias Geiger <elias@ele-dev.de>
2021-05-28 14:42:33 +0200

    Reduced windows script

--- receiver/playStream.bat
+++ receiver/playStream.bat
@@ -1,11 +1,9 @@
 @echo on
 
 :: HEVC UDP stream 
-gst-launch-1.0.exe udpsrc port=5000 ! queue ! application/x-rtp,media=video,encoding-name=H264,payload=96 ! rtpjitterbuffer ! rtph264depay ! queue ! h264parse ! d3d11h264dec ! autovideosink 
-
-:: gst-launch-1.0.exe udpsrc port=5000 ! application/x-rtp,media=video,encoding-name=H265,payload=96 ! rtph265depay ! d3d11h265dec ! d3d11videosink
+gst-launch-1.0.exe udpsrc port=5000 ! queue ! application/x-rtp,media=video,encoding-name=h264,payload=96 ! rtpjitterbuffer ! rtph264depay ! queue ! h264parse ! d3d11h264dec ! autovideosink
 
 :: HEVC TCP stream (not working yet)
-:: gst-launch-1.0.exe tcpclientsrc host=192.168.178.62 port=5001 ! h265parse ! avdec_h265 ! autovideosink
+:: gst-launch-1.0.exe tcpclientsrc host=192.168.178.62 port=5001 ! h264parse ! avdec_h264 ! autovideosink
 
 :: Pause
